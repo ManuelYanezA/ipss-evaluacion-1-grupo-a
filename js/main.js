9aca9f8df8238650bc8c48e287dc104fbc36b40c
ManuelYanezA <manuelyanez.ar@gmail.com>
2026-05-14 18:21:37 -0400

    refactor: modificada funcion para cambiar el tema del navbar

--- js/main.js
+++ js/main.js
@@ -1,27 +1,42 @@
+const lightBtn = document.getElementById("lightThemeBtn");
+const darkBtn = document.getElementById("darkThemeBtn");
+const redBtn = document.getElementById("redThemeBtn");
+
+lightBtn.addEventListener("click", () => {
+    setNavbarTheme("light");
+});
+
+darkBtn.addEventListener("click", () => {
+    setNavbarTheme("dark");
+});
+
+redBtn.addEventListener("click", () => {
+    setNavbarTheme("red");
+});
+
 function setNavbarTheme(theme) {
-        const navbar = document.getElementById("mainNavbar");
+    const navbar = document.getElementById("mainNavbar");
+    navbar.classList.remove(
+        "navbar-light-theme",
+        "navbar-dark-theme",
+        "navbar-red-theme"
+    );
 
-        navbar.classList.remove(
-            "navbar-light-theme",
-            "navbar-dark-theme",
-            "navbar-red-theme"
-        );
-
-        if (theme === "light") {
-            navbar.classList.add("navbar-light-theme");
-            console.log("Tema claro aplicado");
-        }
-
-        if (theme === "dark") {
-            navbar.classList.add("navbar-dark-theme");
-            console.log("Tema oscuro aplicado");
-        }
-
-        if (theme === "red") {
-            navbar.classList.add("navbar-red-theme");
-            console.log("Tema rojo aplicado");
-        }
+    if (theme === "light") {
+        navbar.classList.add("navbar-light-theme");
+        console.log("Tema claro aplicado");
     }
+
+    if (theme === "dark") {
+        navbar.classList.add("navbar-dark-theme");
+        console.log("Tema oscuro aplicado");
+    }
+
+    if (theme === "red") {
+        navbar.classList.add("navbar-red-theme");
+        console.log("Tema rojo aplicado");
+    }
+}
 
 let cartCount = 0;
 const cartButton = document.getElementById("addToCartBtn");
