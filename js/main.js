3aab112c5b0b99d69f9a404faa87e9d78896e68e
ManuelYanezA <manuelyanez.ar@gmail.com>
2026-05-25 12:17:56 -0400

    feat: implementada funcion de sanitizacion

--- js/main.js
+++ js/main.js
@@ -68,3 +68,12 @@ async function mostrarProductos(categoria) {
 }
 mostrarProductos("beauty");
 mostrarProductos("groceries");
+
+function escapeHTML(str) {
+    return String(str)
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#39;')
+}
